aba9b5d5cdc8f4d787c29a07224b0f5571d09e39
Adam Ciesielski <ciesielskiadam6@gmail.com>
2018-10-16 17:33:52 +0200

    poprawa czytelnosci

--- Lab2.cc
+++ Lab2.cc
@@ -2,12 +2,19 @@
 #include <iostream>
 using namespace std;
 
-//To jest komentaż do tego pliku. Ten plik jest ewidentnie źle fsormatowany i należy to koniecznie poprawić, pamiętając o nazwach zmiennych
-//dopuszczalnej długości linii, znakach z kodu ANSI, wcięciach i dbalości o wsykoiehj jakosci kometarze
+/*To jest komentarz do tego pliku. Ten plik jest ewidentnie źle sformatowany 
+  i należy to koniecznie poprawić, pamiętając o nazwach zmiennych 
+  dopuszczalnej długości linii, znakach z kodu ANSI, wcięciach i 
+  dbalości o wysokiej jakosci komentarze*/
 
-int main() { int PierwszaZmienna=5; int Drugazmienna=7;
-int WynikDziałania=PierwszaZmienna+Drugazmienna;
+int main() {
+    int PierwszaZmienna = 5;
+    int Drugazmienna = 7;
+    int WynikDzialania = PierwszaZmienna + Drugazmienna;
 
-cout <<"Wynik Dodawania Dwóch Liczb To:"<<WynikDziałania<<endl; // Wypisuje wynik dzialania
-return 0;
+    cout << "Wynik dodawania dwóch liczb to: " << WynikDzialania << endl; 
+    /* Wypisuje wynik dzialania*/
+
+    return 0;
 }
+
